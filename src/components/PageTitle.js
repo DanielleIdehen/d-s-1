2260e7ac72e96e442502433483b704f2980c5e82
Danielle <32966775+DanielleIdehen@users.noreply.github.com>
2020-06-22 20:35:04 +0100

    'Margin collapse' was causing gaps between heading this has been removed now

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -13,8 +13,8 @@ class PageTitle extends Component {
       <div>
         <h1>{this.state.text}</h1>        
           <div className="simple-box">
-            <h2>Hello</h2>
-            <h3>World</h3>
+            <h2>Heading 2</h2>
+            <h3>Heading 3</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus sollicitudin risus eu ultricies. Phasellus sagittis aliquam accumsan. Ut vitae accumsan ex. Duis vestibulum non nisi vel luctus. Nullam turpis orci, aliquam vitae eros non, pretium ultricies ex. In congue ipsum in tempor sodales. In hac habitasse platea dictumst. Ut et sem tellus. Aliquam vestibulum, velit at blandit aliquet, sapien leo tincidunt justo, in elementum felis leo et sapien.</p>
           </div>
           <div className="simple-box">
