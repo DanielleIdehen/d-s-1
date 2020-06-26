67608c14410a42a057ea81a69ae6c9783ae062e8
Danielle <32966775+DanielleIdehen@users.noreply.github.com>
2020-06-26 17:26:52 +0100

    removed the simple box css

--- src/components/LargeHeader.js
+++ src/components/LargeHeader.js
@@ -13,6 +13,8 @@ class LargeHeader extends Component{
     <div class="grid-container">
       <div><FontAwesomeIcon icon={faBars} /></div>
       <div><PageTitle/></div>
+      <div></div>
+      <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel pellentesque sem. Integer tortor magna.</div>
     </div>
   )}
 }
