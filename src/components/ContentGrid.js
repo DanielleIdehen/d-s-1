678d535850aec425ec51982251d92a432eefec21
Danielle <32966775+DanielleIdehen@users.noreply.github.com>
2020-06-28 10:13:06 +0100

    updated the grid systems for all media qs

--- src/components/ContentGrid.js
+++ src/components/ContentGrid.js
@@ -9,13 +9,14 @@ class ContentGrid extends Component {
   render() {
     return (
       <div class="content-shapes">
-        <div>About Me</div>
-        <div>Education</div>
-        <div>Current Occupation</div>
-        <div>Job History</div>
-        <div>Hobbies</div>
-        <div>About This Website</div>
-        <div>Contact Information</div>
+        <div class="content1">About Me</div>
+        <div class="content2">Education</div>
+        <div class="content3">Current Occupation</div>
+        <div class="content4">Job History</div>
+        <div class="content5">Hobbies</div>
+        <div class="content6">About This Website</div>
+        <div class="content7">Contact Information</div>
+        <div class="content8">Extra</div>
       </div>
     )}
 }
