e187de4b37b333d7008a5f8ea67cf800a63dec22
Danielle <32966775+DanielleIdehen@users.noreply.github.com>
2020-07-16 21:16:17 +0100

    trying to fix typing

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -5,14 +5,18 @@ class Heading extends Component{
   constructor(props){
     super(props);
     this.state = {
-      'title': 'Hi, I\'m Danielle.\nA full stack\nSoftware Engineer.'
+      'title': 'Hi, I\'m Danielle.',
+      'title2': 'A full stack',
+      'title3': 'Software Engineer.'
     }
   }
   render(){
     return(
-      <div class="heading-container">
-        <div class="main-heading">
-          {this.state.title}
+      <div className="heading-container">
+        <div className="main-heading">
+          <div className="line1">{this.state.title}</div>
+          <div className="line2">{this.state.title2}</div>
+          <div className="line3">{this.state.title3}</div>
         </div>
       </div>
     )
