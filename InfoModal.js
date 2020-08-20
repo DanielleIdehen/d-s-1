13176707d0ae935461b4f4fe8fe85b0cda5eac79
Danielle <32966775+DanielleIdehen@users.noreply.github.com>
2020-08-20 19:54:36 +0100

    created modal file

---
+++ InfoModal.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class InfoModal extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      showModal: true
+    }
+  }
+  render(){
+    return(
+      <div></div>
+    )
+  }
+}
+
+export default InfoModal;
