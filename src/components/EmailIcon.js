18cea019db882991273be9a87bf92aa4e556ad6a
Danielle <32966775+DanielleIdehen@users.noreply.github.com>
2020-08-29 13:13:38 +0100

    added subject and body to the email

--- src/components/EmailIcon.js
+++ src/components/EmailIcon.js
@@ -10,7 +10,13 @@ class EmailIcon extends Component {
 
   render(){
     return(
-      <a href="mailto:danielle.idehen@gmail.com">
+      <a href="mailto:danielle.idehen@gmail.com?subject=Portfolio Website
+      &body=Hi%20Danielle!%0D%0A%0D%0AI%20have%20seen%20your%20porfolio%20and%20I%20would%20like%20to%20speak%20to%20you%20about%20it.
+      %0D%0A
+      *Insert your message*
+      %0D%0A %0D%0A %0D%0A
+      Kind regards,
+      ">
         <div class="email-container">
           <div class="email-icon">
           <FontAwesomeIcon icon={faEnvelope} />
